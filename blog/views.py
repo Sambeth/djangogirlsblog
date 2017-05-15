82677d35fa489fc2f9324e67ba8f24865914d9c2
Sambeth <sambethslim@gmail.com>
2017-05-15 11:48:50 +0100

    added views to create view/edit blog post inside the site

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -20,4 +22,41 @@ def post_detail(request, pk):
         'post': post,
     }
     template = 'blog/post_detail.html'
+    return render(request, template, context)
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    context = {
+        'form': form,
+    }
+    template = 'blog/post_edit.html'
+    return render(request, template, context)
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    context = {
+        'form': form,
+    }
+    template = 'blog/post_edit.html'
     return render(request, template, context)
